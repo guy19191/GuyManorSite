f0c2652fe99aba92a37dd4100a3389c61fa1654c
guy19191 <63824070+guy19191@users.noreply.github.com>
2021-01-02 11:54:49 +0200

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,8 +13,8 @@ export default class Porfolio extends Component {
               return(
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
-                    <a href={item.link}>
                       <img src={`${item.imgurl}`} className="item-img"/>
+                       <a href={item.link}>
                       <div className="overlay">
                         <div className="portfolio-item-meta">
                           <h5>{item.name}</h5>
